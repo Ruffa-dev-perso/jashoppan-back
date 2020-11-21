e0b0f247d54e21a0b8286b67964d660d62e01ef9
TheRott06 <69381950+TheRott06@users.noreply.github.com>
2020-11-21 01:04:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
+const cors = require("./middleware/cors");
 const indexRouter = require("./routes/index");
 const accountRouter = require("./routes/account");
 const productRouter = require("./routes/product");
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(cors.handle);
 app.use("/", indexRouter);
 app.use("/account", accountRouter);
 app.use("/product", productRouter);
