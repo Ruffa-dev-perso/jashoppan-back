d9f21955ae1caec25c77784de5efb19fedb71efc
Ruffa-dev-perso <32415731+Ruffa-dev-perso@users.noreply.github.com>
2020-11-25 23:18:23 +0100

    Update cors.js

--- middleware/cors.js
+++ middleware/cors.js
@@ -7,7 +7,7 @@ const cors = {
   handle: (req, res, next) => {
     res.header(
       "Access-Control-Allow-Origin",
-      process.env.NODE_ENV === 'production' ? "http://localhost:3000/" : "https://ruffa-dev-perso.github.io/"
+      process.env.NODE_ENV === 'production' || "http://localhost:3000/" || "https://ruffa-dev-perso.github.io/"
     );
     res.header(
       "Access-Control-Allow-Headers",
